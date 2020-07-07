3a584f54ee1ac262274c36ceae0d29e801bd1c06
BANNITSYNI <illia.bannitsyn@stazysta.comarch.pl>
2020-07-07 17:01:07 +0200

    added tests for telepad loading

--- src/app/misc/misc.component.spec.ts
+++ src/app/misc/misc.component.spec.ts
@@ -1,14 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MiscComponent } from './misc.component';
+import { CalculatorComponent } from '../calculator/calculator.component';
 
 describe('MiscComponent', () => {
   let component: MiscComponent;
+  let com:CalculatorComponent
   let fixture: ComponentFixture<MiscComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MiscComponent ]
+      declarations: [ MiscComponent,CalculatorComponent ]
     })
     .compileComponents();
   }));
@@ -16,10 +18,24 @@ describe('MiscComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(MiscComponent);
     component = fixture.componentInstance;
+    var tmp=TestBed.createComponent(CalculatorComponent)
+    com=tmp.componentInstance;
+    tmp.detectChanges();
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('loading telepad position',()=>
+  {
+    component.loadTelepad(222,222);
+    expect(com.Telepad.x==222&&com.Telepad.y==222).toBeTruthy();
+    component.loadTelepad(5,10);
+    expect(com.Telepad.x==1&&com.Telepad.y==222).toBeFalsy();
+    
+  })
+  
+
+
 });
