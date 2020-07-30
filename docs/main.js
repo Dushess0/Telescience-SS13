c72090a3e020f3d4bf72e5f745a127cd6f711d98
Dushess0 <ilyabannila@gmail.com>
2020-07-30 14:59:33 +0200

    updated build

--- docs/main.js
+++ docs/main.js
@@ -1164,7 +1164,7 @@ _angular_platform_browser__WEBPACK_IMPORTED_MODULE_3__["platformBrowser"]().boot
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Learning Angular\calculator\src\main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! C:\Users\Comarch\Documents\GitHub\Telescience-SS13\src\main.ts */"./src/main.ts");
 
 
 /***/ })
